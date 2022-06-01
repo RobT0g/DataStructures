f85b4567ddfc98caa2649a360999c8d835c09a64
Robson Luan <64618248+RobT0g@users.noreply.github.com>
2022-06-01 17:20:41 -0300

    Update Main.cpp

--- Project1/Main.cpp
+++ Project1/Main.cpp
@@ -2,22 +2,66 @@
 #include "Lista.h"
 
 int main() {
-	Lista list = Lista::Lista();
-	list.insertAt(0, 1);
-	list.push(11);
-	list.insert(10);
-	list.insert(12);
-	list.insert(13);
-	list.push(12);
-	list.insertAt(5, 7);
-	list.insertAt(9, 3);
-	list.printAll();
-	list.removeFirst();
-	list.removeLast();
-	list.removeAt(4);
-	list.printAll();
-	list.~Lista();
-	printf("OK");
+	//Lista list = Lista::Lista();
+	int num, pos, opcao;
+	do{
+		printf("\n---------------------------------------------------\n);
+		printf("01 - Imprimir Lista;\n");
+		printf("02 - Inserir no inicio;\n");
+		printf("03 - Inserir no meio;\n");
+		printf("04 - Inserir no fim;\n");
+		printf("05 - Remover do inicio;\n");
+		printf("06 - Remover do meio;\n");
+		printf("07 - Remover do fim;\n");
+		printf("08 - Buscar elemento;\n");
+		printf("09 - Tamanho da lista;\n");
+		printf("0 - Finalizar;\n");
+		scanf("%d", &opcao);
+		if(opcao > 0 && opcao < 10){
+			switch(opcao){
+				case 1:
+					list.printAll();
+					break;
+				case 2:
+					printf("Insira o valor a ser inserido: ");
+					scanf("%d", &num);
+					list.insert(num);
+					break;
+				case 3:
+					printf("Insira o valor a ser inserido: ");
+					scanf("%d", &num);
+					printf("Insira a posição na qual será inserido: ");
+					scanf("%d", &pos);
+					list.insertAt(num, pos);
+					break;
+				case 4:
+					printf("Insira o valor a ser inserido: ");
+					scanf("%d", &num);
+					list.push(num);
+					break;
+				case 5:
+					list.removeFirst();
+					break;
+				case 6:
+					printf("Insira a posição na qual será retirado: ");
+					scanf("%d", &pos);
+					list.removeAt(pos);
+					break;
+				case 7:
+					list.removeLast();
+					break;
+				case 8:
+					printf("Insira o valor a ser buscado: ");
+					scanf("%d", &num);
+					if(list.inlist(num) != 0)
+						printf("O valor está na lista.\n");
+					else
+						printf("O valor nao esta na lista.\n");
+				case 9:
+					printf("O tamanho e %d.", list.getTam());
+			}
+		}
+	} while(opcao != 0);
 
 	return 0;
 }
