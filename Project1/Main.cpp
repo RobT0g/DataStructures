c2d2424277526079594aaa9198d3562646bbbf16
Robson Luan <64618248+RobT0g@users.noreply.github.com>
2022-06-01 17:24:39 -0300

    Update Main.cpp

--- Project1/Main.cpp
+++ Project1/Main.cpp
@@ -2,10 +2,10 @@
 #include "Lista.h"
 
 int main() {
-	Lista list = Lista::Lista();
+	Lista list = Lista();
 	int num, pos, opcao;
 	do{
-		printf("\n---------------------------------------------------\n);
+		printf("\n---------------------------------------------------\n");
 		printf("01 - Imprimir Lista;\n");
 		printf("02 - Inserir no inicio;\n");
 		printf("03 - Inserir no meio;\n");
@@ -17,49 +17,49 @@ int main() {
 		printf("09 - Tamanho da lista;\n");
 		printf("0 - Finalizar;\n");
 		scanf("%d", &opcao);
-		if(opcao > 0 && opcao < 10){
-			switch(opcao){
-				case 1:
-					list.printAll();
-					break;
-				case 2:
-					printf("Insira o valor a ser inserido: ");
-					scanf("%d", &num);
-					list.insert(num);
-					break;
-				case 3:
-					printf("Insira o valor a ser inserido: ");
-					scanf("%d", &num);
-					printf("Insira a posição na qual será inserido: ");
-					scanf("%d", &pos);
-					list.insertAt(num, pos);
-					break;
-				case 4:
-					printf("Insira o valor a ser inserido: ");
-					scanf("%d", &num);
-					list.push(num);
-					break;
-				case 5:
-					list.removeFirst();
-					break;
-				case 6:
-					printf("Insira a posição na qual será retirado: ");
-					scanf("%d", &pos);
-					list.removeAt(pos);
-					break;
-				case 7:
-					list.removeLast();
-					break;
-				case 8:
-					printf("Insira o valor a ser buscado: ");
-					scanf("%d", &num);
-					if(list.inlist(num) != 0)
-						printf("O valor está na lista.\n");
-					else
-						printf("O valor nao esta na lista.\n");
-				case 9:
-					printf("O tamanho e %d.", list.getTam());
-			}
+		switch(opcao){
+			case 1:
+				list.printAll();
+				break;
+			case 2:
+				printf("Insira o valor a ser inserido: ");
+				scanf("%d", &num);
+				list.insert(num);
+				break;
+			case 3:
+				printf("Insira o valor a ser inserido: ");
+				scanf("%d", &num);
+				printf("Insira a posição§Ã£o na qual será inserido: ");
+				scanf("%d", &pos);
+				list.insertAt(num, pos);
+				break;
+			case 4:
+				printf("Insira o valor a ser inserido: ");
+				scanf("%d", &num);
+				list.push(num);
+				break;
+			case 5:
+				list.removeFirst();
+				break;
+			case 6:
+				printf("Insira a posição na qual será retirado: ");
+				scanf("%d", &pos);
+				list.removeAt(pos);
+				break;
+			case 7:
+				list.removeLast();
+				break;
+			case 8:
+				printf("Insira o valor a ser buscado: ");
+				scanf("%d", &num);
+				if(list.inlist(num) != 0)
+					printf("O valor está na lista.\n");
+				else
+					printf("O valor nao esta na lista.\n");
+			case 9:
+				printf("O tamanho e %d.", list.getTam());
+			default:
+				printf("ERRO! Opção inválida.");
 		}
 	} while(opcao != 0);
 
